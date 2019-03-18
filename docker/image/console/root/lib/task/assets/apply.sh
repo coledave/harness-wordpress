a8d665a74821e1e2914d69da29abd82c036516d1
test <dcole@inviqa.com>
2019-03-18 14:58:46 +0000

    fix regression in assets script

--- docker/image/console/root/lib/task/assets/apply.sh
+++ docker/image/console/root/lib/task/assets/apply.sh
@@ -22,6 +22,6 @@ function task_assets_apply()
 
     for file in "/app/${ASSETS_DIR}/"*.files.{tgz,tar.gz}; do
         [ -f "$file" ] || continue
-        run "tar -zxvf ${file} -C /app"
+        run "tar -xvf ${file} -C /app"
     done
 }
